4bacdbd9b4782326dc837ecb8fd7e8f97c618e7f
LilyanAhmet <l.ahmetoglu@qimia.de>
2019-10-25 17:37:06 +0300

    impelementing blog

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,21 +3,61 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import styles from "../components/css/single-blog.module.css"
 import {Link} from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { log } from "util"
 const Blog = ({ data }) => {
   const {
     title,
     published,
     text: { json },
   } = data.post
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <div className="rich">
+            <h3>this is awesome image</h3>
+            <img width="400" src={node.data.target.fields.file["en-US"].url} />
+         
+          </div>
+        )
+      },
+      "embedded-entry-block": node => {
+        const { title, image, text } = node.data.target.fields
+        console.log(text)
 
+        return (
+          <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1>this is other post : {title["en-US"]}</h1>
+            <img
+              width="400"
+              src={image["en-US"].fields.file["en-US"].url}
+              alt=""
+            />
+            {documentToReactComponents(text["en-US"])}
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        )
+      },
+    },
+  }
   return (
     <Layout>
       <section className={styles.blog}>
         <div className={styles.center}>
           <h1>{title}</h1>
           <h4>published at : {published}</h4>
-
-          <Link  to="/blog" className="btn-primary">
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
+          <Link fade to="/blog" className="btn-primary">
             all posts
           </Link>
         </div>
