f9c0dcc24823e6c312ffa8cf608b6d39132fb69a
LilyanAhmet <l.ahmetoglu@qimia.de>
2019-10-30 10:16:36 +0300

    test for rich box

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -16,7 +16,6 @@ const Blog = ({ data }) => {
       "embedded-asset-block": node => {
         return (
           <div className="rich">
-            <h3>this is awesome image</h3>
             <img width="400" src={node.data.target.fields.file["en-US"].url} />
          
           </div>
--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -6,6 +6,7 @@ import Banner from "../components/Banner"
 import styles from "../components/css/template.module.css"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Day from "../components/SingleTour/Day"
 const Template = ({ data }) => {
   const {
@@ -13,13 +14,49 @@ const Template = ({ data }) => {
     day,
     description: { description },
     gallery,
+    text: { json },
     schedual,
   } = data.tours
 
   const [mainImage, ...tourImages] = gallery
   console.log(mainImage)
   console.log(tourImages)
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <div className="rich">
+            <img width="400" src={node.data.target.fields.file["en-US"].url} />
+         
+          </div>
+        )
+      },
+      "embedded-entry-block": node => {
+        const { title, image, text } = node.data.target.fields
+        console.log(text)
 
+        return (
+          <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1>this is other post : {title["en-US"]}</h1>
+            <img
+              width="400"
+              src={image["en-US"].fields.file["en-US"].url}
+              alt=""
+            />
+            {documentToReactComponents(text["en-US"])}
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        )
+      },
+    },
+  }
   return (
     <Layout>
       <StyledHero>
@@ -46,6 +83,7 @@ const Template = ({ data }) => {
             <h2>{title}</h2>
             <h4>Day : {day}</h4>
             <p className={styles.desc}>{description}</p>
+            {documentToReactComponents(json, options)}
             <h2>Activities schedule</h2>
             <div className={styles.schedual}>
               {schedual.map((item, index) => {
